77088a6dd29010f9db39688797db94f8b66fd4f5
Michael Grandison <african.genetic.survival@gmail.com>
2020-04-12 17:00:46 -0500

    deploy build 5

---
+++ frontend/build/precache-manifest.f63c81f7a8adb95dcc5cee82544a8009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6ec4f2dd007f49afaa2cef96bcc57c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e55006c1168bfe47e640",
+    "url": "/static/css/2.9e813ccd.chunk.css"
+  },
+  {
+    "revision": "d17f2e28e3a0e3493f3e",
+    "url": "/static/css/main.11cb1151.chunk.css"
+  },
+  {
+    "revision": "e55006c1168bfe47e640",
+    "url": "/static/js/2.8e1ae757.chunk.js"
+  },
+  {
+    "revision": "73ad55933f7b8e8f072b6076b2b2af9a",
+    "url": "/static/js/2.8e1ae757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17f2e28e3a0e3493f3e",
+    "url": "/static/js/main.31a80dd7.chunk.js"
+  },
+  {
+    "revision": "f44cb117e82d28f1b796",
+    "url": "/static/js/runtime-main.a855dcb5.js"
+  }
+]);

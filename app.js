fdd8789200aeb9de97d5abdef875fb46fdd570d2
Michael Grandison <african.genetic.survival@gmail.com>
2020-05-08 17:11:32 -0500

    more get creds access testing 4

--- app.js
+++ app.js
@@ -147,5 +147,5 @@ const url = 'https://s3.amazonaws.com/school.africangeneticsurvival.net/assets/c
 
 request.get(url, ( error, response, body) => {
   // pocketVariables.mailjet = body;
-  console.log('beep',body,body.mailjet,body.a);
+  console.log('beep',body,JSON.parse(body));
 });

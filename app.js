05982d0ec4b4c57269a90ca244a139507fede993
Michael Grandison <african.genetic.survival@gmail.com>
2020-05-08 17:59:27 -0500

    switched s3 bucket + test ec2 deploy 4

--- app.js
+++ app.js
@@ -147,6 +147,7 @@ const url = 'https://mycouchcoachstorage.s3.amazonaws.com/assets/creds/mailjet/m
 
 request.get(url, ( error, response, body) => {
   // pocketVariables.mailjet = JSON.parse(body);
+  console.log('beep',body.slice(0,3));
   console.log('beep',body.slice(0,3) === '<?x');
   console.log('boop',body.slice(0,3) === '{"a');
 });
